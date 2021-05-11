ebf9126f53de5efa5675801508f2a7cb1490315e
Daniel Larsen <dalars@microsoft.com>
2021-05-11 14:49:24 +1200

    Audit VNet Route All Enabled

---
+++ Policies/App Service/vnet-route-all-enabled/deploy-policy.ps1
@@ -0,0 +1,14 @@
+$ErrorActionPreference = 'Stop'
+
+$rg = 'vnetrouteall-rg'
+
+$definition = New-AzPolicyDefinition `
+    -Name 'audit-vnetrouteall-enabled' `
+    -DisplayName 'Audit App Service vnetRouteAllEnabled' `
+    -Description 'Audits that vnetRouteAllEnabled is true, ensuring all traffic originating from App is routed through VNet integration' `
+    -Policy 'https://raw.githubusercontent.com/Azure/Community-Policy/master/Policies/App%20Service/vnet-route-all-enabled/azurepolicy.rules.json' `
+    -Parameter 'https://raw.githubusercontent.com/Azure/Community-Policy/master/Policies/App%20Service/vnet-route-all-enabled/azurepolicy.parameters.json' `
+    -Mode Indexed
+
+$scope = Get-AzResourceGroup -Name $rg
+New-AzPolicyAssignment -Name "$rg-audit-vnetrouteall-enabled" -Scope $scope.ResourceId -PolicyDefinition $definition
