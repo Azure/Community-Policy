04809486e361d469a2bf5c433fb107f497f8ee23
Heinrich Gantenbein (techlake) <heinrich.gantenbein@microsoft.com>
2023-05-02 14:08:43 -0500

    Checkpoint

--- Format-BulkPolicies.ps1
+++ Format-BulkPolicies.ps1
@@ -9,7 +9,21 @@ param (
 #TODO: Check if folder has 3 files
 
 # Get all folders in Policies folder and create folders in policyDefinitions folder
-$folders = Get-ChildItem -Path ./Policies -Directory
+$folders1 = Get-ChildItem -Path ./Policies -Directory
+foreach ($folder1 in $folders1) {
+    $folderName1 = $folder1.Name
+    $folderPath1 = "./policyDefinitions/$folderName1"
+    if (!(Test-Path -Path $folderPath1)) {
+        New-Item -Path $folderPath1 -ItemType Directory
+    }
+
+    $folders2 = Get-ChildItem -Path $folderPath1 -Filter "*.json" | Where-Object { $_.Name -notmatch 'azurepolicy.json|azurepolicy.parameters.json|azurepolicy.rules.json' } | Select-Object Directory -Unique
+    foreach ($folder2 in $folders2) {
+        $folderName2 = $folder2.Name
+        if (!(Test-Path -Path $folderPath)) {
+            New-Item -Path $folderPath -ItemType Directory
+        }
+    }
+}
 
 
-Get-ChildItem -Path "C:\Folder" -Recurse -Filter "*.json" | Where-Object { $_.Name -notmatch 'azurepolicy.json|azurepolicy.parameters.json|azurepolicy.rules.json' } | Select-Object Directory -Unique
