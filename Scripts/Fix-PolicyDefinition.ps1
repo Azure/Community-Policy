0fcdd31d93dc6561830f5b605b01a2fc2b177343
SebastianClaesson <SebastianClaesson@hotmail.com>
2023-06-15 20:01:48 +0200

    Fixing logic around guid check.

--- Scripts/Fix-PolicyDefinition.ps1
+++ Scripts/Fix-PolicyDefinition.ps1
@@ -127,10 +127,12 @@ function Confirm-PolicyPropertyDefinition {
                     $currentParameters = $_.properties['Parameters']
                     $_.properties.add('parameters', $currentParameters) | Out-Null
                     $_.properties.remove('Parameters') | Out-Null
-                } elseif ($null -eq $_.properties['parameters']) {
+                }
+                elseif ($null -eq $_.properties['parameters']) {
                     $_.properties.remove('parameters') | Out-Null
                     $_.properties.add('parameters', @{}) | Out-Null
-                } else {
+                }
+                else {
                     $_.properties.add('parameters', @{}) | Out-Null
                 }
             }
@@ -139,7 +141,8 @@ function Confirm-PolicyPropertyDefinition {
                 if ($_.properties['policyrule']) {
                     $_.properties.add('policyRule', $_.properties['policyrule']) | Out-Null
                     $_.properties.remove('policyrule') | Out-Null
-                } elseif ($_.properties['PolicyRule']) {
+                }
+                elseif ($_.properties['PolicyRule']) {
                     $_.properties.add('policyRule', $_.properties['PolicyRule']) | Out-Null
                     $_.properties.remove('PolicyRule') | Out-Null
                 }
@@ -155,17 +158,19 @@ function Confirm-PolicyPropertyDefinition {
             Write-Verbose "Current effect is set to: '$currentEffect', will update the parameter 'effect' accordingly."
             if ($null -eq $policyDefinition.properties['parameters'].Keys) {
                 $policyDefinition.properties.remove('parameters') | Out-Null
-                $policyDefinition.properties.Add('parameters',@{'effect' = @{
-                    "type"          = "string"
-                    "metaData"      = @{
-                        "displayName" = "Policy effect"
-                        "description" = $allowedValuesSets["$($currentEffect.Tolower())"].description
-                    }
-                    "allowedValues" = $allowedValuesSets["$($currentEffect.Tolower())"].allowedValues
-                    "defaultValue"  = $allowedValuesSets["$($currentEffect.Tolower())"].defaultValue
+                $policyDefinition.properties.Add('parameters', @{'effect' = @{
+                            "type"          = "string"
+                            "metaData"      = @{
+                                "displayName" = "Policy effect"
+                                "description" = $allowedValuesSets["$($currentEffect.Tolower())"].description
+                            }
+                            "allowedValues" = $allowedValuesSets["$($currentEffect.Tolower())"].allowedValues
+                            "defaultValue"  = $allowedValuesSets["$($currentEffect.Tolower())"].defaultValue
                 
-                }}) | Out-Null
-            } else {
+                        }
+                    }) | Out-Null
+            }
+            else {
                 $currentParameters = $policyDefinition.properties['parameters']
                 $currentParameters.add('effect', @{
                         "type"          = "string"
@@ -270,7 +275,7 @@ function Confirm-PolicyDefinition {
                 $definition.Add('name', $((new-guid).Guid)) | Out-Null
             }
             { $_['name'] } {
-                if (!$_['name'] -eq [guid]) {
+                if ($_['name'] -notmatch ("^(\{){0,1}[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}(\}){0,1}$")) {
                     Write-verbose 'Name is not a GUID, Updating name.'
                     $definition.remove('name') | Out-Null
                     $definition.Add('name', $((new-guid).Guid))  | Out-Null
@@ -290,6 +295,17 @@ function Confirm-PolicyDefinition {
 
             }
         }
+        New-item -Path $filePath -ItemType File -Value $([ordered]@{
+                'name'       = $definition.name
+                'type'       = $definition.type
+                'properties' = $definition.properties
+            } | ConvertTo-Json -depth 100) -Force | Out-Null
+        Write-Verbose "New Azure Policy restructed at '$filePath'" -Verbose
+    }
+    if ($definition['name'] -notmatch ("^(\{){0,1}[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}(\}){0,1}$")) {
+        Write-verbose 'Name is not a GUID, Updating name.'
+        $definition.remove('name') | Out-Null
+        $definition.Add('name', $((new-guid).Guid))  | Out-Null
         New-item -Path $filePath -ItemType File -Value $([ordered]@{
                 'name'       = $definition.name
                 'type'       = $definition.type
